2e3b93f80e1cb145c00669bc9d21af13627f0f20
Benrabah Mohamed <benrabahmohamed530@gmail.com>
2020-11-20 15:07:31 +0100

    update1
    show view

---
+++ cinemaRoomManager/Main.java
@@ -0,0 +1,20 @@
+package cinemaRoomManager;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Cinema:");
+		
+		System.out.println("  1 2 3 4 5 6 7 8");
+		for (int i = 0; i < 7; i++) {
+			System.out.print(i+1);
+			for (int j = 0; j < 8; j++) {
+				System.out.print(" S");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
