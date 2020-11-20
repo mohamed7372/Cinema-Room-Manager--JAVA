691d4047f8014c8213a9b83c867660df9a18b3ee
Benrabah Mohamed <benrabahmohamed530@gmail.com>
2020-11-20 16:07:56 +0100

    update3
    chose place and give him price of place

--- cinemaRoomManager/Main.java
+++ cinemaRoomManager/Main.java
@@ -8,36 +8,56 @@ public class Main {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
 		System.out.println("Enter the number of rows:");
 		int r = sc.nextInt();
 		System.out.println("Enter the number of seats in each row:");
 		int c = sc.nextInt();
 		
-		System.out.println("Total income:");
-		if(c * r <= 60) {
-			System.out.println("$" + 10 * c * r);
+		String[][] arr = new String[r][c];
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[0].length; j++) {
+				arr[i][j] = "S";
+			}
 		}
-		else {
-			int fr = r/2 * c * 10;
-			int sr = (r-(r/2)) * c * 8;
-			System.out.println("$" + (fr + sr));
-		}
+		print(arr);
 		
+		System.out.println("\nEnter a row number:");
+		int chr = sc.nextInt();
+		System.out.println("Enter a seat number in that row:");
+		int chc = sc.nextInt();
 		
+		System.out.print("\nTicket price: ");
+		System.out.println("$" + price(r, c, chr, chc));
 		
-		/*
+		reserve(arr, chr, chc);
+	}
+	public static void reserve(String[][] arr, int a ,int b) {
+		arr[a-1][b-1] = "B";
+		print(arr);
+	}
+	public static void print(String[][] arr){
+		System.out.println();
 		System.out.println("Cinema:");
-		
-		System.out.println("  1 2 3 4 5 6 7 8");
-		for (int i = 0; i < 7; i++) {
+		System.out.print(" ");
+		for (int i = 0; i < arr[0].length; i++) {
+			System.out.print(" " + (i+1));			
+		}
+		System.out.println();
+		for (int i = 0; i < arr.length; i++) {
 			System.out.print(i+1);
-			for (int j = 0; j < 8; j++) {
-				System.out.print(" S");
+			for (int j = 0; j < arr[0].length; j++) {
+				System.out.print(" " + arr[i][j]);
 			}
 			System.out.println();
-		}
-		*/
+		}		
 	}
-
+	public static int price(int r, int c, int a, int b) {
+		if(r * c <= 60)
+			return 10;
+		else {
+			if(r/2 * c >= a * c)
+				return 10;
+			return 8;
+		}
+	}
 }
