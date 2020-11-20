451ce9e0e442e89f212aa25d10a2f4e3ce75fd1d
Benrabah Mohamed <benrabahmohamed530@gmail.com>
2020-11-20 15:19:35 +0100

    update2
    calculate price of alll tickets

--- cinemaRoomManager/Main.java
+++ cinemaRoomManager/Main.java
@@ -1,9 +1,32 @@
 package cinemaRoomManager;
 
+import java.util.Scanner;
+
 public class Main {
 
+	static Scanner sc = new Scanner(System.in);
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println("Enter the number of rows:");
+		int r = sc.nextInt();
+		System.out.println("Enter the number of seats in each row:");
+		int c = sc.nextInt();
+		
+		System.out.println("Total income:");
+		if(c * r <= 60) {
+			System.out.println("$" + 10 * c * r);
+		}
+		else {
+			int fr = r/2 * c * 10;
+			int sr = (r-(r/2)) * c * 8;
+			System.out.println("$" + (fr + sr));
+		}
+		
+		
+		
+		/*
 		System.out.println("Cinema:");
 		
 		System.out.println("  1 2 3 4 5 6 7 8");
@@ -14,7 +37,7 @@ public class Main {
 			}
 			System.out.println();
 		}
-		
+		*/
 	}
 
 }
